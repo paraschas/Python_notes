a47daef64833f8623bc289eae991380d7ab73592
Dimitrios Paraschas <paraschas@gmail.com>
2016-05-07 22:31:25 +0300

    add template files for Python 2 and Python 3

---
+++ template-Python_2.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+# Dimitrios Paraschas (paraschas@gmail.com)
+
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+
+def main():
+    """
+    main function
+    """
+    print("Hello World!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ template-Python_3.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Dimitrios Paraschas (paraschas@gmail.com)
+
+
+def main():
+    """
+    main function
+    """
+    print("Hello World!")
+
+
+if __name__ == "__main__":
+    main()
