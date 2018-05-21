f22059c01c71e9540159506b5af49367d38d51da
Dimitrios Paraschas <paraschas@gmail.com>
2018-05-21 05:14:41 +0300

    add the read_html function

--- Pandas_notes.py
+++ Pandas_notes.py
@@ -10,6 +10,8 @@ import pandas as pd
 def check_if_dataframe_is_empty():
     """
     Check whether a dataframe is empty.
+
+    http://pandas.pydata.org/pandas-docs/stable/generated/pandas.DataFrame.empty.html
     """
     df = pd.DataFrame()
 
@@ -19,11 +21,26 @@ def check_if_dataframe_is_empty():
         print(df)
 
 
+def read_html():
+    """
+    Generate a list of dataframes from tables in an HTML page.
+
+    https://pandas.pydata.org/pandas-docs/stable/generated/pandas.read_html.html
+    """
+    link = "https://docs.python.org/3/library/functions.html"
+    match = "Built-in Functions"
+    df_list = pd.read_html(io=link, match=match)
+
+    print(df_list[0])
+
+
 def main():
     """
     main function
     """
-    check_if_dataframe_is_empty()
+    # check_if_dataframe_is_empty()
+
+    read_html()
 
 
 if __name__ == "__main__":
